2ec1c01efdac3fe3d1633b77c3e2c56fbbe7c7dd
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-11-03 20:56:46 +0100

    Creating code for first line of result

---
+++ index.js
@@ -0,0 +1,10 @@
+// Creating a variable to store the age value
+let edad = 21;
+
+// Creating conditional code
+if (edad >= 21) {
+    console.log("Eres mayor de edad.");
+} else {
+    console.log("Eres menor de edad.");
+}
+
