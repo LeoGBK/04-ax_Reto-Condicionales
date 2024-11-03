542c48a8961071d9235a362659534371a41b175c
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-11-03 21:04:48 +0100

    Creating code for second line of result

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ if (edad >= 21) {
     console.log("Eres menor de edad.");
 }
 
+// Creating variable to store string variable
+let accion = "crear";
+
+// Using switch to decide which type of text will be printed
+switch (accion) {
+    case "crear":
+        console.log(`La acción es ${accion}.`);
+        break;
+    case "colocar":
+        console.log(`La acción es ${accion}.`);
+        break;
+    case "actualizar":
+        console.log(`La acción es ${accion}.`);
+        break;
+    case "borrar":
+        console.log(`La acción es ${accion}.`);
+        break;
+    default:
+        console.log("No hay ninguna acción.");
+}
